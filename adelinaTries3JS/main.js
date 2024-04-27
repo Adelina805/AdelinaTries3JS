3b2e280c9bb89fdc4a19ba25010b92dfaa35bcc6
Adelina <adelinaballerina1@gmail.com>
2024-04-26 18:00:17 -0700

    trying again

--- adelinaTries3JS/main.js
+++ adelinaTries3JS/main.js
@@ -1,6 +1,6 @@
-import './style.css'
+//import './style.css'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as THREE from 'three'; // use three.js library
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'; // movement
 
 // SETUP
 
